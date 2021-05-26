df01ea8d6723b879dafc00a8090bb7c25d56149c
jellyfish <jvamsif223@gmail.com>
2021-05-26 12:54:36 +0530

    added sass

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Board from './components/Board.js'
+import './styles/root.scss'
 
-export default () => (
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+const App = () => {
+  return(
+    <div className="app">
+      <h1>TIC TAC TOE</h1>
+      <Board/>
+    </div>
+  );
+};
+
+export default App;
